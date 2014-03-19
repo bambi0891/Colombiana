adadabf5f70f6e2bfbd5dcceff21bb62f97d693c
Diego <diegoct6@gmail.com>
2014-03-19 09:02:40 -0500

    Agregar "Probando"

--- ingSoftware/src/main/java/com/colombiana/ingsoftware/MainActivity.java
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 
 //HOlaaaaaaaa
+//MKON!!!!1
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Probando...");
     }
 
 
