28301ae967c01f0bed411f844b2a45f8b2025b5b
Diego <diegoct6@gmail.com>
2014-03-19 09:42:17 -0500

    Se Agrego Splash Screen Se Agrego Intent al LogIn

---
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.colombiana.ingsoftware;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //Se quita ActionBar para LogIn
+    }
+
+
+
+}
