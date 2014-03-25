9de6ba4e24000b76910a67bd9071e4c9036692b5
Diego <diegoct6@gmail.com>
2014-03-25 17:48:28 -0500

    Validación Completa de Registro

--- ingSoftware/src/main/java/com/colombiana/ingsoftware/RegisterActivity.java
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.colombiana.ingsoftware;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,19 +28,76 @@ public class RegisterActivity extends Activity {
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                boolean validador;
                 if((!nombre.getText().toString().equals(""))&&(!apellido.getText().toString().equals(""))&&
                    (!cedula.getText().toString().equals(""))&&(!celular.getText().toString().equals(""))&&
                    (!usuario.getText().toString().equals(""))&&(!password.getText().toString().equals(""))&&
                    (!password2.getText().toString().equals(""))){
-                    new AlertDialog.Builder(RegisterActivity.this).setTitle("Congratulations!")
-                            .setMessage("Registro Exitoso!")
-                            .setNeutralButton("Ok",new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
 
-                                }
-                            }).show();
+                    if(celular.getText().length()==10) {
+                        if (password.getText().toString().equals(password2.getText().toString())) {
+                            if(password.getText().length()>=8){
+                                validador=true;
+                            }
+                            else{
+                                validador = false;
+                                password.setText("");
+                                password.setHintTextColor(Color.RED);
+                                password2.setText("");
+                                password2.setHintTextColor(Color.RED);
+                                new AlertDialog.Builder(RegisterActivity.this).setTitle("Error!")
+                                        .setMessage("La contraseña debe tener por lo menos 8 caracteres!")
+                                        .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                            }
+                                        }).show();
+                            }
+                        }
+                        else{
+                            validador = false;
+                            password.setText("");
+                            password.setHintTextColor(Color.RED);
+                            password2.setText("");
+                            password2.setHintTextColor(Color.RED);
+                            new AlertDialog.Builder(RegisterActivity.this).setTitle("Error!")
+                                    .setMessage("Las contraseñas no coinciden!")
+                                    .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        }
+                                    }).show();
+                        }
+                    }
+                    else{
+                        validador = false;
+                        celular.setText("");
+                        celular.setHintTextColor(Color.RED);
+                        new AlertDialog.Builder(RegisterActivity.this).setTitle("Error!")
+                                .setMessage("El No. Celular debe contener 10 digitos!")
+                                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                }).show();
+                    }
+
+
+                    if(validador) {
+                        new AlertDialog.Builder(RegisterActivity.this).setTitle("Congratulations!")
+                                .setMessage("Registro Exitoso!")
+                                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                    }
+                                }).show();
+                    }
                 }
+
                 else{
                     new AlertDialog.Builder(RegisterActivity.this).setTitle("Error!")
                                                                   .setMessage("Uno o más campos vacios, ingrese todos los datos!")
@@ -51,6 +109,7 @@ public class RegisterActivity extends Activity {
                     }).show();
                 }
             }
+
         });
 
     }
