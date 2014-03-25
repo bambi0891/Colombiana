d1413f44af430a6d7fbfe5559ade8b894b68a3cb
Diego <diegoct6@gmail.com>
2014-03-25 16:35:20 -0500

    Add Layout de Register

--- ingSoftware/src/main/java/com/colombiana/ingsoftware/LoginActivity.java
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.colombiana.ingsoftware;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+
+import android.widget.TextView;
 
 
 public class LoginActivity extends Activity {
@@ -13,7 +17,24 @@ public class LoginActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
+        TextView registro=(TextView)findViewById(R.id.textView4);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(mainIntent);
+            }
+        });
+
     }
+
+
+
+
+
+
+
 
 }
 
