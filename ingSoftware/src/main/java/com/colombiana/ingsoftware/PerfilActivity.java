244a0246b550380146560f6127962c66ccba9397
bambi0891 <javierpile@unisabana.edu.co>
2014-04-01 18:31:15 -0500

    Sistema de ingreso completo, fragments, tabs, perfil!!! y mas :D

---
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/PerfilActivity.java
@@ -0,0 +1,39 @@
+package com.colombiana.ingsoftware;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+
+public class PerfilActivity extends Activity {
+
+    ParseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_perfil);
+
+        user = ParseUser.getCurrentUser();
+
+        TextView nombre = (TextView) findViewById(R.id.textView);
+        TextView celular = (TextView) findViewById(R.id.textView2);
+        TextView cedula = (TextView) findViewById(R.id.textView3);
+        TextView username = (TextView) findViewById(R.id.textView4);
+        Button pass = (Button) findViewById(R.id.button);
+        Button update = (Button) findViewById(R.id.button);
+
+        nombre.setText(nombre.getText() + user.getString("nombre") + " "+user.getString("apellido"));
+        celular.setText(celular.getText()+ user.getString("celular"));
+        cedula.setText(cedula.getText()+ user.getString("cedula"));
+        username.setText(username.getText()+ user.getUsername());
+
+
+    }
+
+}
--- ingSoftware/src/main/java/com/colombiana/ingsoftware/VisitadorActivity.java
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/VisitadorActivity.java
@@ -1,8 +1,13 @@
 package com.colombiana.ingsoftware;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,14 +16,49 @@ import android.view.MenuItem;
 import com.parse.ParseUser;
 
 
-public class VisitadorActivity extends Activity {
+public class VisitadorActivity extends FragmentActivity implements ActionBar.TabListener {
+
+    ViewPager viewPager;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visitador);
 
+        viewPager = (ViewPager) findViewById(R.id.pager);
 
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tab1 = actionBar.newTab();
+        tab1.setIcon(R.drawable.store_icon);
+        tab1.setTabListener(this);
+
+        ActionBar.Tab tab2 =  actionBar.newTab();
+        tab2.setIcon(R.drawable.icon_producto);
+        tab2.setTabListener(this);
+
+        actionBar.addTab(tab1);
+        actionBar.addTab(tab2);
     }
 
 
@@ -31,13 +71,15 @@ public class VisitadorActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
+
         switch (item.getItemId()) {
             case R.id.logout:
                 ParseUser.logOut();
                 logOut();
                 Log.d("My", "Salio");
                 return true;
+            case R.id.perfil:
+                perfil();
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -49,4 +91,52 @@ public class VisitadorActivity extends Activity {
         finish();
     }
 
+    private void perfil()
+    {
+        Intent perfil = new Intent(VisitadorActivity.this, PerfilActivity.class);
+        startActivity(perfil);
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction fragmentTransaction) {
+
+    }
+
+    class MyAdapter extends FragmentPagerAdapter
+    {
+        public  MyAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            if(position == 0)
+            {
+                fragment = new Tienda();
+            }
+            if(position == 1)
+            {
+                fragment = new Producto();
+            }
+
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
 }
