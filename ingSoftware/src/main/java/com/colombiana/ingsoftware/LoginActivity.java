6d1e42a5bfee5f1138c0d9cd363441bf917df084
juandiaz1209 <juandiaz1209@hotmail.com>
2014-04-02 08:31:00 -0500

    holaa

--- ingSoftware/src/main/java/com/colombiana/ingsoftware/LoginActivity.java
+++ ingSoftware/src/main/java/com/colombiana/ingsoftware/LoginActivity.java
@@ -8,7 +8,8 @@ import android.view.Window;
 public class LoginActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         requestWindowFeature(Window.FEATURE_NO_TITLE); //Se quita ActionBar para LogIn
